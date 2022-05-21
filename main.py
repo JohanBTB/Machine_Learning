9171ad1dcc620d749a06f511803128154d4cf35f
Johan <56638242+JohanBTB@users.noreply.github.com>
2022-05-20 23:24:40 -0500

    Modelos hechos

--- main.py
+++ main.py
@@ -80,19 +80,106 @@ banco.replace(valores_yes_no, inplace = True)
 banco['contact'] = banco['contact'].map(lambda x: 0 if x=='unknown' or x == 0 else 1 )
 
 # banco['month'].unique()
-dict_month = {}
+dict_mes = {}
 from datetime import datetime
-for month in banco['month'].unique():
-    datetime_month = datetime.strptime(month, '%b')
-    num_month = datetime_month.month
-    dict_month[month] = num_month
-banco.replace(dict_month, inplace = True)
+for mes in banco['month'].unique():
+    datetime_mes = datetime.strptime(mes, '%b')
+    num_mes = datetime_mes.month
+    dict_mes[mes] = num_mes
+banco.replace(dict_mes, inplace = True)
 
 banco['poutcome'] = banco['poutcome'].map(lambda x: 0 if x!= 'success' else 1)
 
-# import seaborn as sns
-# correlation = banco.corr()
-# sns.heatmap(correlation, cmap = "RdBu", vmin = -1, center = 0)
+# ======================= FIN LIMPIEZA DATOS ==================================
+
+# ======================= CORRELACION DE DATOS ===============================
+
+import seaborn as sns
+correlation = banco.corr()
+sns.heatmap(correlation, cmap = "RdBu", vmin = -1, center = 0)
+
+# import matplotlib.pyplot as plt
+
+# temp_banco = banco.groupby('age', as_index = False).mean()
+# plt.scatter(data = temp_banco, x='age', y='job')
+# plt.title('Edad vs Trabajo')
+# plt.xlabel('edad')
+# plt.ylabel('trabajo')
+
+
+# plt.scatter(data = temp_banco, x='age', y='marital')
+# plt.title('Edad vs Matrimonio')
+# plt.xlabel('edad')
+# plt.ylabel('matrimonio')
+
+
+
+banco.pop('job')
+banco.pop('marital')
+
+banco.pop('day')
+banco.pop('month')
+banco.pop('campaign')
+banco.pop('credit_card')
+banco.pop('loan')
+banco.pop('pdays')
+# ======================FIN CORRELACION DE DATOS =============================
+
+# ====================== LOGISTIC REGRESSION =================================
+
+# from sklearn import preprocessing
+# from sklearn.pipeline import make_pipeline
+# from sklearn.linear_model import LogisticRegression
+
+# x_train = banco.sample(frac = 0.7, random_state=3)
+# x_test = banco.drop(x_train.index)
+
+# y_train = x_train.pop('y')
+# y_test = x_test.pop('y')
+
+# pipe = make_pipeline(preprocessing.StandardScaler(), LogisticRegression(solver='lbfgs', max_iter=20))
+# pipe.fit(x_train,y_train)
+# pipe.fit(x_test,y_test)
+# print(pipe.score(x_test,y_test))
+
+# ======================= FIN LOGISTIC REGRESSION =============================
+
+# ========================== ARBOL DE DECISION ================================
+
+
+# x_train = banco.sample(frac = 0.7, random_state=3)
+# x_test = banco.drop(x_train.index)
+
+# y_train = x_train.pop('y')
+# y_test = x_test.pop('y')
+
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.metrics import accuracy_score
+# dt = DecisionTreeClassifier()
+
+# dt.fit(x_train, y_train)
+
+# y_predict = dt.predict(x_test)
+
+# dt_accuracy = accuracy_score(y_test, y_predict)
+
+# ======================== FIN ARBOL DE DECISION ==============================
+
+from sklearn.naive_bayes import GaussianNB
+
+
+x_train = banco.sample(frac = 0.7, random_state=3)
+x_test = banco.drop(x_train.index)
+
+y_train = x_train.pop('y')
+y_test = x_test.pop('y')
+
+
+gnb = GaussianNB()
+gnb.fit(x_train, y_train)
+y_predict = gnb.predict(x_test)
+gnb_accuracy = gnb.score(x_train,y_train)
+print("El score accuracy es: {}".format(round(gnb_accuracy, 10)))
 
 
 
